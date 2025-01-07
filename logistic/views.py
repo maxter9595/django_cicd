c9c4c085bbc06b3694b2df0d8142a24b67a56757
Maxim Terletskii <max.t95@bk.ru>
2025-01-07 18:35:56 +0300

    Changed views.py

--- logistic/views.py
+++ logistic/views.py
@@ -25,4 +25,4 @@ class StockViewSet(ModelViewSet):
 
 class TestView(APIView):
     def get(self, request):
-        return Response({"message": "HELLO WORLD!"})
+        return Response({"message": "HELLO WORLD!!!"})
