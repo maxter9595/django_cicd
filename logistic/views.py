3d496a417179f4fce673a0d5a917f95cd8ddd624
Maxim Terletskii <max.t95@bk.ru>
2025-01-08 23:47:00 +0300

    Changed views.py test text

--- logistic/views.py
+++ logistic/views.py
@@ -25,4 +25,4 @@ class StockViewSet(ModelViewSet):
 
 class TestView(APIView):
     def get(self, request):
-        return Response({"message": "Hello domain name, Baby!"})
+        return Response({"message": "Hello russian domain name, Baby!"})
