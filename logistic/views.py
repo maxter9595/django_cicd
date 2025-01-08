e911a065468ec9a9c76048046b4a99d4a71d6689
Maxim Terletskii <max.t95@bk.ru>
2025-01-08 21:19:36 +0300

    Added domain name check

--- logistic/views.py
+++ logistic/views.py
@@ -25,4 +25,4 @@ class StockViewSet(ModelViewSet):
 
 class TestView(APIView):
     def get(self, request):
-        return Response({"message": "Всем большой привет от разработчика!"})
+        return Response({"message": "Hello domain name!"})
